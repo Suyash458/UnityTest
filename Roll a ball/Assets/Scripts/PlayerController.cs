5cc598e9c4a85dacd5a3f7e2664cbb4bb1a738ca
Suyash458 <Sne9x@outlook.com>
2015-06-02 01:27:51 +0530

    Added walls and collectables.

--- Roll a ball/Assets/Scripts/PlayerController.cs
+++ Roll a ball/Assets/Scripts/PlayerController.cs
@@ -6,14 +6,23 @@ public class PlayerController : MonoBehaviour {
 	private Rigidbody rb;
 	public float speed;
 	
-	void Start(){
+	void Start()
+	{
 		rb = GetComponent<Rigidbody> ();
 	}
 	
-	void FixedUpdate(){
+	void FixedUpdate()
+	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		rb.AddForce (movement * speed);	
+	}
+	void OnTriggerEnter (Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick Up")) 
+		{
+			other.gameObject.SetActive (false);
+		}
 	}
 }
